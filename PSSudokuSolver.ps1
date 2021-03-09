4ee2c01fd264a5612bc9efa80775b523a5df7193
Ben1492 <redkadett@gmail.com>
2021-03-09 16:36:40 -0700

    it's working

--- PSSudokuSolver.ps1
+++ PSSudokuSolver.ps1
@@ -1,5 +1,4 @@
 function show-board() {
-    
     param (
         $board
     )
@@ -9,22 +8,22 @@ function show-board() {
         for ($x = 0; $x -lt 9; $x++) {
             if ($x % 3 -eq 0) {
                 $outputLine += "|"
-            } 
+            }
             $outputLine += $board[$y][$x]
             $outputLine += " "
         }
 
+
         if ($y % 3 -eq 0) {
             "- - - - - - - - - -"
         }
-        $outputLine 
-
+        $outputLine
     }
-
 
 }
 
 function solve-puzzle {
+
     param (
         $board
     )
@@ -33,22 +32,21 @@ function solve-puzzle {
 
     if ($null -eq $foundCell) {
         return $true
-    } else {
+    }
+    else {
         $row = $foundCell[1]
         $col = $foundCell[0]
     }
 
-    for ($i=1; $i -lt 10; $i++) {
+    for ($i = 1; $i -lt 10; $i++) {
         if (validate-cell -board $board -num $i -cell $foundCell) {
             $board[$row][$col] = $i
-
-            if (solve-puzzle show-board $board) {
+            if (solve-puzzle -board $board) {
                 return $true
             }
 
             $board[$row][$col] = 0
         }
-
     }
 
     return $false
@@ -60,33 +58,32 @@ function validate-cell {
         $num,
         $cell
     )
-    
-    #validate the row    
-    for ($x=0; $x -lt 9; $x++ ) {
-        if ($board[$cell[1]][$x] -eq $num -and $cell[1] -ne $num) {
+
+    #validate the row   
+    for ($x = 0; $x -lt 9; $x++ ) {
+        if ($board[$cell[1]][$x] -eq $num -and $cell[0] -ne $x) {
             return $false
         }
     }
 
-    #validate the column    
-    for ($y=0; $y -lt 9; $y++ ) {
-        if ($board[$y][$cell[0]] -eq $num -and $cell[0] -ne $num) {
+    #validate the column   
+    for ($y = 0; $y -lt 9; $y++ ) {
+        if ($board[$y][$cell[0]] -eq $num -and $cell[1] -ne $y) {
             return $false
         }
     }
 
     #check box
-    $box_x = $cell[1] % 3
-    $box_y = $cell[0] % 3
+    $box_x = [math]::floor($cell[0] / 3)
+    $box_y = [math]::floor($cell[1] / 3)
 
-    for ($y=$box_y*3; $y -lt $box_y*3+3 ; $y++ ) {
-        for ($x=$box_x*3; $x -lt $box_x*3+3; $x++ ) {
-            if ($board[$y][$x] -eq $num -and ($x -eq $cell[0] -and $y -eq $cell[1])) {
+    for ($y = $box_y * 3; $y -lt $box_y * 3 + 2 ; $y++ ) {
+        for ($x = $box_x * 3; $x -lt $box_x * 3 + 2; $x++ ) {
+            if ($board[$y][$x] -eq $num -and ($x -ne $cell[0] -and $y -ne $cell[1])) {
                 return $false
             }
         }
     }
-
     return $true
 }
 
@@ -98,33 +95,29 @@ function find-empty {
     for ($y = 0; $y -lt 9; $y++) {
         for ($x = 0; $x -lt 9; $x++) {
             if ($board[$y][$x] -eq 0) {
-                return $x,$y
+                return $x, $y
             }
         }
     }
-
     return $null
 }
 
 
-[System.Collections.ArrayList]$puzzle = @(
-    @(0,0,0,0,0,9,2,0,8),
-    @(5,0,0,8,0,2,7,6,0),
-    @(0,8,0,6,0,0,1,0,0),
-    @(0,0,2,0,8,0,0,1,0),
-    @(0,9,0,0,7,0,0,5,0),
-    @(0,3,0,0,9,0,8,0,0),
-    @(0,0,5,0,0,8,0,2,0),
-    @(0,1,8,9,0,5,0,0,7),
-    @(6,0,3,1,0,0,0,0,0)
+
+$puzzle = @(
+    @(0, 0, 0, 0, 0, 9, 2, 0, 8),
+    @(5, 0, 0, 8, 0, 2, 7, 6, 0),
+    @(0, 8, 0, 6, 0, 0, 1, 0, 0),
+    @(0, 0, 2, 0, 8, 0, 0, 1, 0),
+    @(0, 9, 0, 0, 7, 0, 0, 5, 0),
+    @(0, 3, 0, 0, 9, 0, 8, 0, 0),
+    @(0, 0, 5, 0, 0, 8, 0, 2, 0),
+    @(0, 1, 8, 9, 0, 5, 0, 0, 7),
+    @(6, 0, 3, 1, 0, 0, 0, 0, 0)
 )
-
-
 
 show-board -board $puzzle
 solve-puzzle -board $puzzle
 "---------------------------------------------"
 ""
 show-board -board $puzzle
-
-
