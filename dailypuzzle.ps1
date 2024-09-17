4f35079a7da28600f729ad05b6c869c97b5b8aaf
Ben Jacobs <Ben.Jacobs@srpnet.com>
2024-09-17 11:11:44 -0700

    add new puzzle for 9.15.2024

--- dailypuzzle.ps1
+++ dailypuzzle.ps1
@@ -1,26 +1,38 @@
-# 8/12/2024
-# A number increased by 20% and the the result decreased by 20%. The result is 10 less than the original number
-foreach ($originalNumber in 1..500) {
-    $newNumber = $originalNumber * 1.2
-    $finalNumber = $newNumber * .8
-    
-    if ($finalNumber -eq $originalNumber - 10) {
-        "It's $originalNumber"
+# 9/15/2024
+# Zajac had 3x the $$ of Hiroko. Zoroko gave $28 to Hiroko now H has 5x $$ as Zajac
+
+foreach ($hiroko in 1..1000) {
+    $zajac = $hiroko * 3
+    $newZajac = $zajac - 28
+    $newHiroko = $hiroko + 28
+    if ($newHiroko -eq ($newZajac * 5)) {
+        " Hiroko: $hiroko Zajac: $zajac"
     }
 }
+
+# 8/12/2024
+# A number increased by 20% and the the result decreased by 20%. The result is 10 less than the original number
+# foreach ($originalNumber in 1..500) {
+#     $newNumber = $originalNumber * 1.2
+#     $finalNumber = $newNumber * .8
+    
+#     if ($finalNumber -eq $originalNumber - 10) {
+#         "It's $originalNumber"
+#     }
+# }
 
 # 6/17/2024
 
-foreach ($javierNow in 1..100) {
-    $eduardoNow = $javierNow * 6
-    $javierIn12 = $javierNow + 12
-    $eduardoIn12 = $eduardoNow + 12
+# foreach ($javierNow in 1..100) {
+#     $eduardoNow = $javierNow * 6
+#     $javierIn12 = $javierNow + 12
+#     $eduardoIn12 = $eduardoNow + 12
 
-    if ($eduardoIn12 -eq ($javierIn12 * 2)) {
-        "Javier: $javierNow"
-        "Eduardo: $eduardoNow"
-    }
-}
+#     if ($eduardoIn12 -eq ($javierIn12 * 2)) {
+#         "Javier: $javierNow"
+#         "Eduardo: $eduardoNow"
+#     }
+# }
 
 # 5/18/2024
 
